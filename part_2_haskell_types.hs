b02bf69825e001f996b6225647fd586bd69d3cda
Brian Pollack <brian@pollack.tech>
2017-04-17 13:51:24 -0400

    Added myreverse function

--- part_2_haskell_types.hs
+++ part_2_haskell_types.hs
@@ -7,5 +7,12 @@ flatten [Element a] = [Element a]
 flatten [SubList a] = (flatten a)
 flatten list = (flatten [(head list)]) ++ (flatten (tail list))
 
+myreverse :: [NestedList a] -> [NestedList a]
+myreverse [] = []
+myreverse [Element a] = [Element a]
+myreverse [SubList a] = [SubList (myreverse a)]
+myreverse list = (myreverse (tail list)) ++ (myreverse [(head list)])
+
 main = do
   print(flatten ([Element 1,Element 3,SubList [Element 4,SubList [SubList [Element 5],SubList []]],Element 6]))
+  print(myreverse ([Element 1,Element 3,SubList [Element 4,SubList [SubList [Element 5],SubList []]],Element 6]))
